b4aac4b0cffc9b44c94b69a98f413ab47834752f
xd <ldawid100@onet.pl>
2022-03-23 10:34:00 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+$db = new mysqli('localhost','root','','debug_test');
+
+$id = 1;
+$q = $db->prepare("SELECT * FROM test WHERE id = ?");
+$q->bind_param('i', $id);
+if($q->execute()) {
+    //ta część wykona się tylko przy udanej kwerendzie
+    $result = $q->get_result();
+    $row = $result->fetch_assoc();
+
+}
+
+?>
